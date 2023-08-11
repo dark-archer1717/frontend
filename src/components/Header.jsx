b014744094afa6a457838871297131a944c90333
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2023-08-11 10:06:27 +0600

    frtnd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ import { Popover, Transition } from '@headlessui/react'
 import clsx from 'clsx'
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
-import { Logo } from '@/components/Logo'
 import { NavLink } from '@/components/NavLink'
 
 function MobileNavLink({ href, children }) {
@@ -95,7 +94,6 @@ export function Header() {
         <nav className="relative z-50 flex justify-between">
           <div className="flex items-center md:gap-x-12">
             <Link href="#" aria-label="Home">
-              <Logo className="h-10 w-auto" />
             </Link>
             <div className="hidden md:flex md:gap-x-6">
               <NavLink href="#features">Products</NavLink>
